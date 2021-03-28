4e6da938f65573833d9aa49adb43102cea9a82d3
DirkM <pilotbloggeer73@gmail.com>
2021-03-28 21:51:37 +0200

    new update text

--- src/components/App.js
+++ src/components/App.js
@@ -42,7 +42,7 @@ export default function App() {
     <div>
       <p>
         {format(addDays(new Date(), 1), 'EEEE d-MMM')} there will be{' '}
-        <strong>{results.length}</strong> Hola die Waldfee
+        <strong>{results.length}</strong> Meteoriten am Startstst
       </p>
       <hr></hr>
       {results
